710779d6b862849948c600af2bb1c487cd2f5ac6
Chelsea Durazo <chelsea.durazo@encora.com>
2024-07-08 00:58:23 -0700

    Update ToDoController.java
    cross origins address for the frontend

--- src/main/java/com/encora/ToDoBack/controller/ToDoController.java
+++ src/main/java/com/encora/ToDoBack/controller/ToDoController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 
-@CrossOrigin(origins = "http://192.168.1.69:8080/")
+@CrossOrigin(origins = "http://localhost:8080/")
 @RestController
 public class ToDoController {
 
